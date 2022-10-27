eaea108cae1e9de53c3633ebcffb66ac0da1471e
Hanna Kozak <hannawkozak@gmail.com>
2022-10-27 21:50:39 +0100

    feat: add header section

---
+++ pages/main/script.js
@@ -0,0 +1,10 @@
+const navigationLinks = document.getElementsByClassName("header__link");
+
+// add active state to navigation links on click event
+for (let i = 0; i < navigationLinks.length; i++) {
+  navigationLinks[i].addEventListener("click", function () {
+    let current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
